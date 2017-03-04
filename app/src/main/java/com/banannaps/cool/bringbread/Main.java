8b0c7f61363aaad112684fdfaa877e3096477581
Aleix <aleixsanchis@hotmail.com>
2017-03-04 09:54:41 +0100

    accessing gps

--- app/src/main/java/com/banannaps/cool/bringbread/Main.java
+++ app/src/main/java/com/banannaps/cool/bringbread/Main.java
@@ -1,5 +1,7 @@
 package com.banannaps.cool.bringbread;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -37,7 +39,7 @@ public class Main extends AppCompatActivity {
 
         setBtnAfegir();
         setListView();
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
     }
