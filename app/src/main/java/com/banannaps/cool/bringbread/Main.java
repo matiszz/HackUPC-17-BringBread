ef0ba5d67b79585c34bfc64ff5d21aafa0b8e1fa
matiasszarfer <matias.szbar@gmail.com>
2017-03-04 10:54:43 +0100

    Probant notificacio...

--- app/src/main/java/com/banannaps/cool/bringbread/Main.java
+++ app/src/main/java/com/banannaps/cool/bringbread/Main.java
@@ -1,10 +1,15 @@
 package com.banannaps.cool.bringbread;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.ArrayMap;
 import android.util.Log;
@@ -14,15 +19,19 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static android.app.Notification.PRIORITY_MAX;
+
 public class Main extends AppCompatActivity {
     ListView lv_tasques;
     FloatingActionButton btn_afegir;
+    Button btnNotif;
 
     // El map Llocs com a clau el Nom del lloc, i un Vecotr de 2 doubles amb latitud i longitud.
     public static ArrayMap<String, double[]> Llocs = new ArrayMap<>();
@@ -34,6 +43,61 @@ public class Main extends AppCompatActivity {
 
         setBtnAfegir();
         setListView();
+
+        /////////////////////
+
+        btnNotif = (Button) findViewById(R.id.btnNotific);
+        btnNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rebreMissatge();
+            }
+        });
+
+        /////////////////////
+    }
+
+    private void rebreMissatge() {
+
+        int mId = 0;
+
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher_round)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!")
+                        .setPriority(PRIORITY_MAX);
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        String[] events = new String[6];
+        // Sets a title for the Inbox in expanded layout
+        inboxStyle.setBigContentTitle("Event tracker details:");
+        // Moves events into the expanded layout
+        for (int i=0; i < events.length; i++) {
+
+            inboxStyle.addLine(events[i]);
+        }
+        // Moves the expanded layout object into the notification object.
+        mBuilder.setStyle(inboxStyle);
+
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, Main.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(Main.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(mId, mBuilder.build());
     }
 
     private void setBtnAfegir() {
--- app/src/main/java/com/banannaps/cool/bringbread/firebasemessage.java
+++ app/src/main/java/com/banannaps/cool/bringbread/firebasemessage.java
@@ -18,32 +18,6 @@ public class firebasemessage extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage message){
 
-        int mId = 0;
-
-        NotificationCompat.Builder mBuilder =
-                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
-                .setPriority(PRIORITY_MAX);
-
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, Main.class);
-
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(Main.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT );
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        // mId allows you to update the notification later on.
-        mNotificationManager.notify(mId, mBuilder.build());
 
     }
 }
